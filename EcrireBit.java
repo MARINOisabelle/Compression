0d3c5ae9a4f492ccf27f13e6268b108dbf430cc6
llabad39 <lucaslabadens@hotmail.fr>
2016-03-30 19:02:01 +0200

    ecrire bit a bit

---
+++ EcrireBit.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class EcrireBit extends FilterOutputStream{
+    int octet;
+    int nbBit;
+    
+    public EcrireBit(OutputStream out){
+	super(out);
+	this.nbBit=0;
+	this.octet=0x00;
+    }
+    public void writeBit(int bit) throws IOException{
+	int mask = 0x80;
+	mask = mask>>nbBit;
+	if(bit==1){
+	    octet = octet|mask;
+	}
+	nbBit++;
+	if(nbBit==8){
+	    write(octet);
+	    nbBit=0;
+	    this.octet=0x00;
+	}
+    }
+}
