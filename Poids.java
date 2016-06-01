90e61c363dd135fe759afed56596996ad2624e7c
lucas labadens <lucaslabadens@MacBook-Pro-de-lucas.local>
2016-06-01 16:29:27 +0200

    j

--- Poids.java
+++ Poids.java
@@ -5,5 +5,5 @@ public abstract class Poids{
     abstract public void affiche();//affiche l'arbre binaire en dfs
     abstract public void dfs(ArrayList<Integer> a, CodeLettre[] cl);//remplit un tableau avec tout les codes de compression de chaque caractère
     abstract public int ListArbre(ArrayList<Integer> l,int pere,boolean gauche);//écrit l'abre binaire de compression dans une liste
-    
+    abstract public int comptePoids();
 }
